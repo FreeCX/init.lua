fa7875165e596f03732eab9ecfa49ec8f71f415b
Alexey Golubev <dr.freecx@gmail.com>
2022-12-31 10:55:31 +0300

    feat: autoremove trailing spaces

---
+++ lua/config/autocmd.lua
@@ -0,0 +1,17 @@
+-- trip trailing spaces on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    -- use only this type of files
+    pattern = {
+        -- Rust, Python, Lua, Haskell, Bash
+        "*.rs", "*.py", "*.lua", "*.hs", "*.sh",
+        -- C, C++
+        "*.c", "*.cc", "*.cpp", "*.cxx", "*.h", "*.hh", "*.hpp",
+        -- SVG, HTML, CSS, Javascript
+        "*.html", "*.htm", "*.css", "*.js",
+        -- Markdown, YAML, TOML
+        "*.md", "*.yml", "*.yaml", "*.toml",
+        -- Dockerfile, Makefile, Pipfile, meson
+        "Dockerfile", "Makefile", "Pipfile", "*.build",
+    },
+    command = [[%s/\s\+$//e]],
+})
--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,4 +1,4 @@
 require("config.set")
 require("config.packer")
 require("config.theme")
-
+require("config.autocmd")
