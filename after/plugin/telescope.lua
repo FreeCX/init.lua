3f0f6eb55d9678dc3a9d79c4ff5e39ed46f09168
Alexey Golubev <dr.freecx@gmail.com>
2023-01-09 18:58:29 +0300

    chore: tiny tweak

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,4 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
