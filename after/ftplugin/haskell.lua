6a38c2f1e62e7a6a431c534552a6469405eda212
Alexey Golubev <dr.freecx@gmail.com>
2023-01-13 22:32:28 +0300

    chore: custom tabs size by lang

---
+++ after/ftplugin/haskell.lua
@@ -0,0 +1,4 @@
+-- custom indent for haskell
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,4 @@
+-- custom indent for lua
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
