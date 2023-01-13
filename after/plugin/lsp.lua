b011d56811699a0a718f7daf1d15530fd5f0bdc7
Alexey Golubev <dr.freecx@gmail.com>
2023-01-13 22:32:49 +0300

    chore: format

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,10 +1,8 @@
 local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
-
 lsp.ensure_installed({
-    'sumneko_lua',
-    'rust_analyzer'
+  "sumneko_lua",
+  "rust_analyzer"
 })
-
 lsp.setup()
