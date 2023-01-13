5240e5d838fe68fbf042eb7a03235ea4149b5fb1
Alexey Golubev <dr.freecx@gmail.com>
2023-01-13 22:32:05 +0300

    chore: lspconfig

---
+++ after/plugin/lspconfig.lua
@@ -0,0 +1,31 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+local mason_lspconfig = require "mason-lspconfig"
+local servers = {
+  --    clangd = {},
+  --    rust_analyzer = {},
+  sumneko_lua = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+      format = { enable = true }
+    }
+  }
+}
+
+local on_attach = function(_, bufnr)
+  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+    vim.lsp.buf.format()
+  end, { desc = "Format current buffer with LSP" })
+end
+
+mason_lspconfig.setup_handlers {
+  function(server_name)
+    require("lspconfig")[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+    }
+  end,
+}
