55f63dd6e65b10beba8bc916a0714379dddaf255
Alexey Golubev <dr.freecx@gmail.com>
2023-01-14 23:19:51 +0300

    feat: configure bufferline

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,1 +1,11 @@
-require("bufferline").setup {}
+require("bufferline").setup {
+  options = {
+    diagnostics = "nvim_lsp",
+    show_buffer_close_icons = false,
+    separator_style = "slant",
+    name_formatter = function (buf)
+      -- add empty prefix separator
+      return " " .. buf.name
+    end
+  }
+}
