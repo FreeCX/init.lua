e664ee6a3e76532f5443b2df66b98dd347ee2971
Alexey Golubev <dr.freecx@gmail.com>
2023-01-14 21:59:05 +0300

    feat: add telescope fzf

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -5,10 +5,9 @@ return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
 
   -- packages
-  use {
-    "nvim-telescope/telescope.nvim", tag = "0.1.0",
-    requires = { { "nvim-lua/plenary.nvim" } }
-  }
+  use { "nvim-telescope/telescope.nvim", tag = "0.1.x", requires = { { "nvim-lua/plenary.nvim" } } }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+
   use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
   use("lukas-reineke/indent-blankline.nvim")
   use("mbbill/undotree")
