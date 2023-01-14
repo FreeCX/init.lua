5e30c383612413118d70b168ea2b09a6588990ce
Alexey Golubev <dr.freecx@gmail.com>
2023-01-14 23:20:20 +0300

    feat: add buffer change shorcuts

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,1 +1,5 @@
 vim.g.mapleader = " "
+
+-- switch buffers
+vim.keymap.set("n", "<leader>.", ":bnext<CR>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<leader>,", ":bprevious<CR>", { desc = "Previous Buffer" })
