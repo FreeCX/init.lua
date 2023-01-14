74227498f5f910e57e7d6389f7d811fe5c4451a9
Alexey Golubev <dr.freecx@gmail.com>
2023-01-14 19:56:16 +0300

    feat: configure snippet

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,12 @@
 local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
+lsp.setup_nvim_cmp({
+  preselect = "none",
+  completion = {
+      completeopt = "menu,menuone,noinsert,noselect"
+  },
+})
 lsp.ensure_installed({
   "sumneko_lua",
   "rust_analyzer"
