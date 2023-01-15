2e4768ec55bd8dd57cddc409c76d36ea4bb56a5b
Alexey Golubev <dr.freecx@gmail.com>
2023-01-15 13:50:24 +0300

    chore: configure buffers

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,8 +1,8 @@
 require("bufferline").setup {
   options = {
     diagnostics = "nvim_lsp",
+    show_close_icon = false,
     show_buffer_close_icons = false,
-    separator_style = "slant",
     name_formatter = function (buf)
       -- add empty prefix separator
       return " " .. buf.name
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 
 -- switch buffers
-vim.keymap.set("n", "<leader>.", ":bnext<CR>", { desc = "Next Buffer" })
-vim.keymap.set("n", "<leader>,", ":bprevious<CR>", { desc = "Previous Buffer" })
+vim.keymap.set("n", "<leader>]", ":bnext<CR>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<leader>[", ":bprevious<CR>", { desc = "Previous Buffer" })
+vim.keymap.set("n", "<leader>'", ":bd<CR>", { desc = "Close Buffer" })
