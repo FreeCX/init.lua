bbc90b36b02578085b3b62ced8276d6c9952e7b3
Alexey Golubev <dr.freecx@gmail.com>
2023-01-16 11:59:35 +0300

    chore: shortcut for explore

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,3 +4,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>]", ":bnext<CR>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<leader>[", ":bprevious<CR>", { desc = "Previous Buffer" })
 vim.keymap.set("n", "<leader>'", ":bd<CR>", { desc = "Close Buffer" })
+
+vim.keymap.set("n", "<leader>t", ":Ex<CR>", { desc = "Explore" })
