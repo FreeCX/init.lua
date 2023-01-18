2a81b1a0ef1e6fccb36fc7f97efcfa58f88fb261
Alexey Golubev <dr.freecx@gmail.com>
2023-01-18 12:14:04 +0300

    chore: disable icon for bufferline

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,6 +1,7 @@
 require("bufferline").setup {
   options = {
     diagnostics = "nvim_lsp",
+    show_buffer_icons = false,
     show_close_icon = false,
     show_buffer_close_icons = false,
     name_formatter = function (buf)
