2e1f2b45f9429d9f6c668ffbbc5ea014e69b0f0e
Alexey Golubev <dr.freecx@gmail.com>
2023-01-21 13:36:58 +0300

    chore: tweak buffer

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -4,9 +4,5 @@ require("bufferline").setup {
     show_buffer_icons = false,
     show_close_icon = false,
     show_buffer_close_icons = false,
-    name_formatter = function (buf)
-      -- add empty prefix separator
-      return " " .. buf.name
-    end
-  }
+  },
 }
