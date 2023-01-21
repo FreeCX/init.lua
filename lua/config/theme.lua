3244b435a81b7674ab826ab19476a5dc2b826cb0
Alexey Golubev <dr.freecx@gmail.com>
2023-01-21 13:37:17 +0300

    chore: tweak theme comment style

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,2 +1,18 @@
+-- configure theme
+require("nightfox").setup {
+  options = {
+    styles = {
+      -- now comment are bold
+      comments = "bold",
+    },
+  },
+  palettes = {
+    nordfox = {
+      -- tweak comment color #60728A to 20% tint
+      comment = "#808ea1"
+    }
+  }
+}
+
 -- setup theme
 vim.cmd [[ colorscheme nordfox ]]
