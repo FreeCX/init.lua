7d6f1cdd75626589f4834a8944d886ab1ff5a791
Alexey Golubev <dr.freecx@gmail.com>
2023-02-10 22:52:57 +0300

    chore(remap): add key for diagnostic info

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -5,4 +5,8 @@ vim.keymap.set("n", "<leader>]", ":bnext<CR>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<leader>[", ":bprevious<CR>", { desc = "Previous Buffer" })
 vim.keymap.set("n", "<leader>'", ":bd!<CR>", { desc = "Close Buffer" })
 
+-- file explorer
 vim.keymap.set("n", "<leader>t", ":Ex<CR>", { desc = "Explore" })
+
+-- show diagnostic info
+vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, { desc = "Open diagnostic info" })
