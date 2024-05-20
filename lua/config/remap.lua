6e504aa78ca245334058f98af2cdbaabba29f60f
Alexey Golubev <dr.freecx@gmail.com>
2024-05-20 16:13:50 +0300

    chore: remap bindings

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -6,7 +6,13 @@ vim.keymap.set("n", "<leader>[", ":bprevious<CR>", { desc = "Previous Buffer" })
 vim.keymap.set("n", "<leader>'", ":bd!<CR>", { desc = "Close Buffer" })
 
 -- file explorer
-vim.keymap.set("n", "<leader>t", ":Ex<CR>", { desc = "Explore" })
+vim.keymap.set("n", "<leader>t", ":Neotree<CR>", { desc = "Neo-tree" })
 
 -- show diagnostic info
 vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, { desc = "Open diagnostic info" })
+
+-- fine-cmdline
+vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', {noremap = true})
+
+-- show/hide invlist
+vim.keymap.set("n", "<leader>,", ":set invlist<CR>", { desc = "Show/Hide spec chars" })
